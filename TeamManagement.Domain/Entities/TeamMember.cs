3e6338c45e6da4484c79b5282dc7cbcc72c6fad1
iman <dev.imansabet@gmail.com>
2025-07-11 15:23:24 +0330

    TeamMember Entity

---
+++ TeamManagement.Domain/Entities/TeamMember.cs
@@ -0,0 +1,32 @@
+﻿using TeamManagement.Domain.SeedWork;
+
+namespace TeamManagement.Domain.Entities;
+
+public class TeamMember : Entity
+{
+    public TeamMember
+        (int countryId ,
+        int teamLeadId,
+        string email ,
+        string firstname ,
+        string lastname ,
+        string password)
+    {
+        CountryId = countryId ;
+        TeamLeadId = teamLeadId ;
+        Email = email ;
+        FirstName = firstname ;
+        LastName = lastname ;
+        Password = password ;
+    }
+
+    public int CountryId { get; private set; }
+    public int TeamLeadId { get; private set; }
+    public string Email { get;private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Password { get; private set; }
+    public bool IsActive { get; private set; } = true;
+    public DateTime DateCreated { get; private set; } = DateTime.Now;
+    public string Role { get; private set; } = "TEAMMEMBER";
+}
