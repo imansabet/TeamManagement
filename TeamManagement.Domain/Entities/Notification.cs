1c9b532dc126191cbee01d9d80a4b18716d59f8b
iman <dev.imansabet@gmail.com>
2025-07-11 16:06:10 +0330

    Notification Entity

---
+++ TeamManagement.Domain/Entities/Notification.cs
@@ -0,0 +1,20 @@
+﻿using TeamManagement.Domain.SeedWork;
+
+namespace TeamManagement.Domain.Entities;
+
+public class Notification : Entity
+{
+    public Notification(int userId, string role, string title, string description)
+    {
+        UserId = userId;
+        Role = role;
+        Title = title;
+        Description = description;
+    }
+    public int UserId { get; private set; }
+
+    public string Role { get; set; }
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public DateTime DateCreated { get; private set; } = DateTime.Now;
+}
