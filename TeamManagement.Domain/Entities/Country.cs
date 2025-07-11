767c9c1982bdf419d09a3cf6b32c2a76e3358b4e
iman <dev.imansabet@gmail.com>
2025-07-11 14:08:05 +0330

    Add Entity base class , Country class

---
+++ TeamManagement.Domain/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using TeamManagement.Domain.SeedWork;
+
+namespace TeamManagement.Domain.Entities;
+
+public class Country : Entity
+{
+    public Country(string name, bool isActive)
+    {
+        Name = name;
+        IsActive = isActive;
+    }
+    public string Name { get; private set; }
+    public bool IsActive { get; private set; }
+    
+}
---
+++ TeamManagement.Domain/SeedWork/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamManagement.Domain.SeedWork;
+
+public abstract class Entity
+{
+    public virtual int Id { get; protected set; }
+}
