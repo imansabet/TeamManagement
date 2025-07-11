54ca4ee299aa4155490772d072ecc86a96b8232e
iman <dev.imansabet@gmail.com>
2025-07-11 16:11:46 +0330

    Add IGenericRepositoryTEntity Interface

---
+++ TeamManagement.Domain/SeedWork/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace TeamManagement.Domain.SeedWork;
+
+public interface IGenericRepository<T> where T : Entity
+{
+    Task<T> AddAsync(T entity);
+    Task GetByIdAsync(int id);
+    Task Get(Expression<Func<T,bool>> predicate);
+    Task<IReadOnlyList<T>> GetAll();
+    Task UpdateAsync(T entity);
+}
